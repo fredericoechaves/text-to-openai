1941f6adf5e9edafe12d64dcddaca3319c88e961
Frederico Chaves <frederico.chaves@gmail.com>
2023-06-04 16:03:11 -0300

    Usando python-dotenv

--- main.py
+++ main.py
@@ -1,41 +1,27 @@
-import langchain
-from whoosh import index, fields, qparser
-from whoosh.index import create_in
-from whoosh.qparser import QueryParser
-import openai
 import os
+import openai
+from dotenv import load_dotenv
 
-def index_text_files(directory):
-    schema = fields.Schema(content=fields.TEXT(stored=True))
-    if not os.path.exists("index"):
-        os.mkdir("index")
-    ix = create_in("index", schema)
-    writer = ix.writer()
+def concatenate_text_files(directory):
+    concatenated_text = ""
     for filename in os.listdir(directory):
         if filename.endswith('.txt'):
             file_path = os.path.join(directory, filename)
             with open(file_path, 'r') as file:
                 text = file.read()
-                writer.add_document(content=text)
-    writer.commit()
-    return ix
-
-def search_text(index, prompt):
-    with index.searcher() as searcher:
-        query = QueryParser("content", index.schema).parse(prompt)
-        results = searcher.search(query)
-        return results
+                concatenated_text += text
+    return concatenated_text
 
 def get_user_prompt():
     return input("Enter your prompt: ")
 
-def generate_response(prompt):
-    # Replace 'YOUR_OPENAI_API_KEY' with your actual OpenAI API key
-    openai.api_key = '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'
-    
+def generate_response(prompt, text):
+    load_dotenv()
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
     response = openai.Completion.create(
         engine='text-davinci-003',
-        prompt=prompt,
+        prompt=text + prompt,
         max_tokens=100,
         n=1,
         stop=None,
@@ -46,16 +32,14 @@ def generate_response(prompt):
 
 def main():
     directory = 'txt'  # Directory containing the text files
-    chromadb = index_text_files(directory)
-    prompt = get_user_prompt()
-    results = search_text(chromadb, prompt)
-    if results:
-        top_result = results[0]
-        response = generate_response(top_result)
+    concatenated_text = concatenate_text_files(directory)
+    while True:
+        prompt = get_user_prompt()
+        if prompt.lower() in ['quit', 'exit']:
+            break
+        response = generate_response(prompt, concatenated_text)
         print("OpenAI Response:")
         print(response)
-    else:
-        print("No results found.")
 
 if __name__ == '__main__':
     main()
